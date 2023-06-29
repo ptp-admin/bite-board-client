e9d933eb32a03dadce4216a4295a177629077df5
beau-haldane <62363339+beau-haldane@users.noreply.github.com>
2023-06-29 22:47:47 +1000

    improved Ingredient type (#2)

--- types/ingredients.ts
+++ types/ingredients.ts
@@ -1,25 +1,10 @@
-export interface DatabaseIngredient {
+export interface Ingredient {
 	id: number,
-  name: string,
-  category?: string,
-	cost_per?: number,
-  number_of?: number,
-  measurement_unit?: string, // TODO add a type for the unit enum and assign that type to this field
-}
-
-export interface EmptyIngredient {
   name: string,
   category?: string,
 	costPer?: number,
   numberOf?: number,
 	editable: boolean,
-  measurementUnit?: string, // TODO add a type for the unit enum and assign that type to this field
-}
-
-export interface Ingredient extends EmptyIngredient {
-	id: number
-}
-
-export interface SearchableIngredient extends Ingredient {
+  measurementUnit?: string,
 	searchKeywords: string
 }

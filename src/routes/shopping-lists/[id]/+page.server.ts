cd9be3e406d6dee26f49ae7f89caacaa7873358a
beau-cadent <beau.jorgensen@cadent.com.au>
2023-12-06 21:32:52 +1000

    add delete buttons

--- src/routes/shopping-lists/[id]/+page.server.ts
+++ src/routes/shopping-lists/[id]/+page.server.ts
@@ -4,10 +4,10 @@ import { error } from '@sveltejs/kit';
 import type { ShoppingList } from '../../../../types/data';
 
 export const load = (async (event) => {
-	const shoppingList = await axiosHandler({
-		method: 'get',
-		route: `/shopping-lists/${event.params.id}`
-	});
+  const shoppingList = await axiosHandler({
+    method: 'get',
+    route: `/shopping-lists/${event.params.id}`
+  });
 	const recipes = await axiosHandler({
 		method: 'get',
 		route: `/recipes/`
@@ -20,3 +20,17 @@ export const load = (async (event) => {
 	// error if not
 	throw error(404, 'Not found');
 }) satisfies PageServerLoad;
+
+export const actions = {
+  deleteRecipe: async ({ request }) => {
+    const data = await request.formData();
+    const recipeId = data.get('recipeId')
+    const shoppingListId = data.get('shoppingListId');
+
+    const response = await axiosHandler({
+      method: 'delete',
+      route: `/shopping-lists/${shoppingListId}/recipes/${recipeId}`,
+    });
+    return { success: true };
+  },
+};
